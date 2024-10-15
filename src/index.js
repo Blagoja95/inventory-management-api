db0a8648c3a25fd8416b7bef75d4d354d1b9e5d8
Boris Blagojevic <boris.blagojevic@develabs.net>
2024-10-15 13:53:24 +0200

    Documentation route
    Documentation route

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const PORT = process.env.NODE_PORT || 5000;
 
 const app = express();
 const errorHandler = require('./errors/ErrorHandlingMiddleware');
+
 const inventoryRouter = require('./routes/InventoryRoutes');
+const documentationRouter = require('./routes/DocumentationRoute');
 
 app.use(express.json());
 
 app.use(inventoryRouter);
+app.use(documentationRouter);
 
 app.use(errorHandler);
 app.listen(PORT, () => console.info(`Server listening on port: ${PORT}`));
